88e522e54d9694162e1439295e6747eb8af76585
novalsh <muhammad.harjana@binus.ac.id>
2023-02-16 12:03:05 +0700

    pembaruan

--- src/views/product/ProductIndex.vue
+++ src/views/product/ProductIndex.vue
@@ -1,71 +1,37 @@
 <template>
   <div>
-     <div id="page-wrap">
+    <div id="page-wrap">
       <div class="grip-wrap">
-         <div class="product-item"
-         v-for="product in products"
-          :key="product.id"
-         >
-         <img :src="product.imageUrl" alt="" >
-         <h3 class="product-name">{{ product.name }}</h3>
-         <p class="product-price">Rp.{{ product.price }}</p>
-          <router-link :to="{ name: 'product-detail', params: {id: product.id}}">
-          <button>Detail</button>
-        </router-link>
-         </div>
+        <ProductItem
+        v-for="product in products"
+        :key="product.id"
+        :product="product"
+        />
       </div>
-     </div>
+    </div>
   </div>
 </template>
 
 <script>
+import ProductItem from '../../components/ProductItem';
 import { products } from '../../data-seed';
 export default {
   data() {
     return {
       products
     }
+  },
+  components: {
+    ProductItem
   }
-
 }
 </script>
 
 <style scoped >
-  .grip-wrap {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-    margin-top: 16px;
-  }
-
-  .product-item {
-    align-items: center;
-    border-radius: 8px;
-    box-shadow: 0px 2px 5px #888;
-    display: flex;
-    flex-direction: column;
-    flex-wrap: wrap;
-    margin-bottom: 2%;
-    padding: 10px;
-    position: relative;
-    width: 32%;
-  }
-
-  .product-name {
-    margin-bottom: 0;
-  }
-
-  img {
-    height: 200px;
-    width: 200px;
-    border-radius: 5%;
-  }
-
-  a {
-    width: 100%;
-  }
-
-  button {
-    width: 100%;
-  }
+.grip-wrap {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin-top: 16px;
+}
 </style>
