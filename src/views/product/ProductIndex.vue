3319285865015021cd4f39320e686cb461b2a001
novalsh <muhammad.harjana@binus.ac.id>
2023-02-19 14:05:13 +0700

    mengubah data dummy

--- src/views/product/ProductIndex.vue
+++ src/views/product/ProductIndex.vue
@@ -2,24 +2,24 @@
   <div>
     <div id="page-wrap">
       <div class="grip-wrap">
-        <ProductItem
-        v-for="product in products"
-        :key="product.id"
-        :product="product"
-        />
+        <ProductItem v-for="product in products" :key="product.id" :product="product" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import ProductItem from '../../components/ProductItem';
-import { products } from '../../data-seed';
+import ProductItem from '../../components/ProductItem'
+import axios from 'axios'
 export default {
   data() {
     return {
-      products
+      products: []
     }
+  },
+  async created() {
+    const result = await axios.get('http://localhost:8000/api/products')
+    this.products = result.data
   },
   components: {
     ProductItem
